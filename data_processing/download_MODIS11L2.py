8629969c3b872ced75671dc6e769c8877c8ed314
SohierDane <sohier.dane@gmail.com>
2016-07-27 21:55:53 -0700

    progress on MODIS download, adding pycurl to deal with usernm and pass

--- data_processing/download_MODIS11L2.py
+++ data_processing/download_MODIS11L2.py
@@ -1,78 +1,84 @@
 '''
-Downloads all hdf and xml files for the MODIS11L2 product over 
+Downloads all hdf and xml files for the MODIS11L2 product over
 a specified range of dates and specified times of day.
 
-
-Notes on NASA's conventions:
-Incoming file names look like 'MOD11_L2.A2000055.1720.006.2015054160015.hdf'.
-Mapped to useful info:
-MOD11_L2.A    2000     055   17 20      .006.  2015054160015.hdf
-prefix         yr  day_of_yr hr min   version# processing date
-
-url for that file:
-http://e4ftl01.cr.usgs.gov/MOLT/MOD11_L2.006/2000.02.24/
-MOD11_L2.A2000055.1720.006.2015054160015.hdf
-
-<- month and day sections are always len == 2 (01 not 1)
-
-metadata is the same but suffixed .xml after the .hdf
-
+Please note that this is ~4 terabytes of data, if all times of day
+are included.
 '''
 
-# set up to download only specific time of day for specific year to start,
-# so can get 1 usable data first
-# logging?
-# zfill as needed to add zeroes to dates
-
 import requests
-import multiprocessing
 import os.path
+import pycurl
 from urllib import urlretrieve
 from get_constants import get_project_constants
 
-project_constants = get_project_constants()
-first_yr = project_constants['FIRST_YR']
-last_yr = project_constants['LAST_YR']
-ftp_user_id = project_constants['EARTHDATA_USER_ID']
-ftp_pass = project_constants['EARTHDATA_PWD']
-raw_data_dlpath = project_constants['RAW_MODIS11L2_DATA_PATH']
-metadata_dlpath = project_constants['MODIS11L2_METADATA_PATH']
+'''
+curl notes:
+-u == --username
+-L == --location
+-c == --cookie-jar
+-b == --cookie
+'''
 
 
 
-def download_yr(year, hr, min, raw_data_dlpath=raw_data_dlpath,
-                metadata_dlpath=metadata_dlpath, ftp_root_url=ftp_root_url,
-                ftp_user_id=ftp_user_id, ftp_pass=ftp_pass):
+def get_link(year, month, day, time_of_day, urls_dir_path=urls_dir_path):
+    year = year.zfill(4)
+    month = month.zfill(2)
+    day = day.zfill(2)
+    url_file_path = urls_dir_path+'.'.join([year, month, day])
+    with open(url_file_path, 'r') as f_open:
+        pass
+    '''
+    find closest match
+    '''
+
+
+def download_yr(year, time_of_day, raw_data_dlpath, metadata_dlpath,
+                ftp_root_url, ftp_user_id, ftp_pass):
     '''
     Downloads all files in year at a given time, such as all 1300 GMT data from 2004
     '''
 
 
-def download_one_file(year, month, day, hr, min,
-                 raw_data_dlpath=raw_data_dlpath,
-                 metadata_dlpath=metadata_dlpath, ftp_root_url=ftp_root_url,
-                 ftp_user_id=ftp_user_id, ftp_pass=ftp_pass):
+def download_one_file(year, month, day, time_of_day,
+                      raw_data_dlpath, metadata_dlpath,
+                      ftp_user_id, ftp_pass):
     '''
     If the modis file does not already exist, downloads both hdf and xml
     Returns 1 if successful or file already exists, zero if error.
+
+confirmed that this curl command works:
+curl -u username:pass -L -c test_cookie.txt -b test_cookie.txt http://e4ftl01.cr.usgs.gov/MOLT/MOD09A1.006/2001.01.09/MOD09A1.A2001009.h13v01.006.2015140120258.hdf --output MOD09A1.A2001009.h13v01.006.2015140120258.hdf
+
     '''
-    subdir_prexif = str(year)+'.'+str(month)+'.'+str(day)
+    subdir_prexif = '.'.join([str(year),str(month),+str(day)])
     fname = 'dlkjs'
     fpath = raw_data_dlpath+subdir_prexif+fname
     if os.path.isfile(fpath):
         return 1
 
 
-#mirador_dat = requests.get(mirador_dat_url)
-#mirador_dat = mirador_dat.text.strip().split('\n')
-#yrs_to_download = [str(i) for i in range(first_yr, last_yr+1)]
-#
-#for line in mirador_dat:
-#    if sum([1 if i in line else 0 for i in yrs_to_download]) > 0:
-#        '''
-#        Set file name as the last portion of the url.
-#        Should look something like '3B42_Daily.20160429.7.nc4'
-#        '''
-#        fpath = output_directory+line.split('/')[-1]
-#        fpath = fpath.encode('ascii')
-#        urlretrieve(line, fpath)
+
+
+if __name__ == '__main__':
+    project_constants = get_project_constants()
+    first_yr = project_constants['FIRST_YR']
+    last_yr = project_constants['LAST_YR']
+    ftp_user_id = project_constants['EARTHDATA_USER_ID']
+    ftp_pass = project_constants['EARTHDATA_PWD']
+    raw_data_dlpath = project_constants['RAW_MODIS11L2_DATA_PATH']
+    metadata_dlpath = project_constants['MODIS11L2_METADATA_PATH']
+
+    '''
+    cookie file must exist for NASA's ftp system
+    '''
+    cookie_path = metadata_dlpath+'/nasa_cookie.txt'
+    open(cookie_path, 'a').close()
+
+    times_to_dl = [str(x).zfill(2)+'05' for x in range(0, 24)]
+    for time_of_day in times_to_dl:
+        for yr in range(int(first_yr), int(last_yr)+1):
+            download_yr(yr, time_of_day,
+                        raw_data_dlpath, metadata_dlpath,
+                        ftp_user_id, ftp_pass)
