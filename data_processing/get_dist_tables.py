1d116332f5e9254f3e46c03de310808925c4b22e
SohierDane <sohier.dane@gmail.com>
2016-08-01 13:30:22 -0700

    shifted dist_tables to ignore continents

--- data_processing/get_dist_tables.py
+++ data_processing/get_dist_tables.py
@@ -7,19 +7,15 @@ import pandas as pd
 import numpy as np
 from time import time
 from get_constants import get_project_constants
-from country_data import get_country_to_continent_map
 
 
-def haversine_dist(lat1, lon1, lat2, lon2, radius_Earth=6384):
-    # accepts lat/long in degrees, converts to radians
+def haversine_dist(lat1, lon1, lat2, lon2, cos_lat_1, cos_lat_2,
+                   radius_Earth=6384):
+    # expects lat/long in radians
     # default radius of earth is in kilometers
-    lat1 = np.radians(lat1)
-    lat2 = np.radians(lat2)
-    lon1 = np.radians(lon1)
-    lon2 = np.radians(lon2)
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    a = np.sin(dlat/2)**2 + cos_lat_1 * cos_lat_2 * np.sin(dlon/2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
     return radius_Earth * c
 
@@ -31,8 +27,12 @@ def calc_table(df):
     """
     num_stations = len(df.ID.values)
     dists = np.zeros([num_stations, num_stations])
+    counter = 0
     for i in xrange(num_stations-1):
         for j in xrange(i+1, num_stations):
+            counter += 1
+            if counter % 10000 == 0:
+                print "processed "+str(counter)+" out of "+str(num_stations**2)
             distance = haversine_dist(df.LAT.iloc[i], df.LON.iloc[i],
                                       df.LAT.iloc[j], df.LON.iloc[j])
             dists[i, j] = distance
@@ -41,35 +41,35 @@ def calc_table(df):
     return dists
 
 
+def load_metadata(metadata_path):
+    station_metadata_file = 'ish-history.csv'
+    metadata_df = pd.read_csv(os.path.join(
+        metadata_path, station_metadata_file),
+        dtype={'USAF': str, 'WBAN': str})
+    metadata_df['ID'] = metadata_df['USAF']+'-'+metadata_df['WBAN']
+    metadata_df['LAT'] = metadata_df['LAT'].apply(np.radians)
+    metadata_df['LON'] = metadata_df['LON'].apply(np.radians)
+    metadata_df['cos_LAT'] = metadata_df['LAT'].apply(np.cos)
+    return metadata_df
+
+
 def calc_table_2(df):
     dists = {}
     num_stations = len(df.ID.values)
     # add stn_id:[list of ids within 1000 km radius box]
+    pass
 
 
 def calc_dist_tables():
     project_constants = get_project_constants()
     metadata_path = project_constants['GSOD_METADATA_PATH']
     processed_data_path = project_constants['PROCESSED_GROUND_STATION_DATA_PATH']
-    station_metadata_file = 'ish-history.csv'
-    metadata_df = pd.read_csv(os.path.join(
-        metadata_path, station_metadata_file),
-        dtype={'USAF': str, 'WBAN': str})
-    metadata_df['ID'] = metadata_df['USAF']+'-'+metadata_df['WBAN']
     active_stations = os.listdir(processed_data_path)
     end_of_id_idx = active_stations[0].rfind('.')
     active_stations = set([x[:end_of_id_idx] for x in active_stations])
+    metadata_df = load_metadata(metadata_path)
     metadata_df = metadata_df[metadata_df.ID.isin(active_stations)]
-#    dist_tables = {'all': calc_table(metadata_df)}
-    
-    ctry_to_continent = get_country_to_continent_map()
-    metadata_df['Continent'] = metadata_df.CTRY.apply(
-        lambda x: ctry_to_continent[x])
-    continents = {'Europe', 'Africa', 'Asia', 'North America',
-                  'South America', 'Oceania'}
-    dist_tables = {cont: calc_table(metadata_df[
-                    metadata_df.Continent == cont])
-                   for cont in continents}
+    dist_tables = {'all': calc_table(metadata_df)}
     for cont, d_table in dist_tables.iteritems():
         save_path = os.path.join(metadata_path, 'distances_'+cont)
         np.save(save_path, d_table)
