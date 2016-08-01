5cad7b819160d6575bf5092ddb5a9506bdbd1bee
SohierDane <sohier.dane@gmail.com>
2016-07-31 18:22:31 -0700

    sdfs

--- data_processing/clean_grnd_data.py
+++ data_processing/clean_grnd_data.py
@@ -13,7 +13,10 @@ from get_constants import get_project_constants
 def convert_noaa_missing_to_null(df):
     ''' converts noaa codes for missing data to null values'''
     df.replace({'9999.9': nan}, inplace=True)
-    df.replace({'99.99': nan}, inplace=True)
+    try:    
+        df['Precipitation'].replace({'99.99': nan}, inplace=True)
+    except KeyError:
+        print(df.head)
     return df
 
 
