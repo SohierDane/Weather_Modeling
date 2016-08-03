5bee105394bcd3801bb09714a8a93fb3367d1b7f
SohierDane <sohier.dane@gmail.com>
2016-08-03 11:26:22 -0700

    adjusted data cleaning

--- data_processing/clean_grnd_data.py
+++ data_processing/clean_grnd_data.py
@@ -35,7 +35,8 @@ def has_enough_days_complete_day(df):
 def load_station(path):
         column_nms = ['Date', 'Year', 'Month', 'Day', 'Temp', 'STP',
                    'Max_Temp', 'Min_Temp', 'Precipitation', 'Precip_Flag']
-        df = pd.read_csv(path, dtype={col: str for col in column_nms})
+        col_nm_map = {col: str for col in column_nms}
+        df = pd.read_csv(path, dtype=col_nm_map)
         return df
 
 
