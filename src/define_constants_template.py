a31f884082d901db58a65fca3c111b881797537a
SohierDane <sohier.dane@gmail.com>
2016-08-15 10:37:26 -0700

    Updated constants template

--- src/define_constants_template.py
+++ src/define_constants_template.py
@@ -8,15 +8,29 @@ Edit as needed for your data folder organization.
 import json
 
 project_constants = {
-    'FIRST_YR': '',
-    'LAST_YR': '',
-    'RAW_TRMM_3B42_DATA_PATH': '',
-    'RAW_ELEVATION_DATA_PATH': '',
-    'GROUND_STATION_DATA_PATH': '',
-    'RAW_ECOSYSTEM_DATA_PATH': '',
-    'DAT_FILES_PATH': '',
-    'EARTHDATA_USER_ID': '',
-    'EARTHDATA_PWD': ''}
+    "PROCESSED_GROUND_STATION_DATA_PATH": "",
+    "PROCESSED_TRMM_3B43_DATA_PATH": "",
+    "RAW_TRMM_3B43_DATA_PATH": "",
+    "MAX_LATITUDE": 50,
+    "PROCESSED_TRMM_3B42_DATA_PATH": "",
+    "PROCESSED_ELEVATION_DATA_PATH": "",
+    "LAST_YR": "2009",
+    "RAW_GROUND_STATION_DATA_PATH": "",
+    "TRMM_3B42_METADATA_PATH": "",
+    "ELEVATION_METADATA_PATH": "",
+    "RAW_ELEVATION_DATA_PATH": "",
+    "PROCESSED_ECOSYSTEM_DATA_PATH": "",
+    "DAT_FILES_PATH": "",
+    "RAW_ECOSYSTEM_DATA_PATH": "",
+    "EARTHDATA_PWD": "",
+    "FIRST_YR": "1998",
+    "RAW_TRMM_3B42_DATA_PATH": "",
+    "MIN_LATITUDE": -50,
+    "EARTHDATA_USER_ID": "",
+    "ECOSYSTEM_METADATA_PATH": "",
+    "TRMM_3B43_METADATA_PATH": "",
+    "GSOD_METADATA_PATH": ""
+}
 
 
 with open('project_constants.json', 'w') as f_open:
